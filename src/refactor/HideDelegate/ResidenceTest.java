7b3b03fe4efb0226e94e18bb7714e5d0a55744bf
twflu <yzzhou@thoughtworks.com>
2014-01-23 14:17:22 +0800

    5

--- src/refactor/HideDelegate/ResidenceTest.java
+++ src/refactor/HideDelegate/ResidenceTest.java
@@ -1,21 +1,28 @@
 package refactor.HideDelegate;
 
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by twer on 1/23/14.
  */
 public class ResidenceTest {
+
+    private Person _xiaoming;
+    private Person _managerOfA;
+
+    @Before
+    public void beforeEach(){
+        _xiaoming = new Person("_xiaoming");
+        _managerOfA = new Person("managerOfA");
+        Department departmentA = new Department("A", _managerOfA);
+        _xiaoming.setDepartment(departmentA);
+    }
+
     @Test
-    public void should_get_correct_department_manager(){
-
-        Person xiaoming = new Person("xiaoming");
-        Person managerOfA = new Person("managerOfA");
-        Department departmentA = new Department("A", managerOfA);
-
-        xiaoming.setDepartment(departmentA);
-
-        Assert.assertEquals("managerOfA", xiaoming.getDepartment().getManager().getName());
+    public void should_get_correct_department_manager_name(){
+        Department xiaomingDepartment = _xiaoming.getDepartment();
+        Assert.assertEquals(_managerOfA, xiaomingDepartment.getManager());
     }
 }
